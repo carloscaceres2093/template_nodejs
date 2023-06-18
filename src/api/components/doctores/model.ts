a8e6c18d2e86359ce9bc09a0121e69302f4a6f0a
ShadowCrypt97 <sergio970228@gmail.com>
2023-06-18 15:42:44 -0500

    modelo doctor

---
+++ src/api/components/doctores/model.ts
@@ -0,0 +1,10 @@
+export interface Doctor {
+    id_doctor: number,
+    nombre: string,
+    apellido: string,
+    especialidad: string,
+    consultorio: string,
+    correo: string,
+    created_at: string,
+    updated_at: string
+} 
